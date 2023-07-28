bc2c0fc9bebb2d9304d65e2974503e6df8b28d84
Engels Souza <engels.franca@icloud.com>
2023-07-28 00:06:45 -0300

    aula3 - parte1

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
 
+from fast_zero.schemas import UserDB, UserPublic, UserSchema
+
 app = FastAPI()
 
 
 @app.get('/')
 def read_root():
     return {'message': 'Olá Mundo!'}
+
+
+database = []   # provisório para estudo!
+
+
+@app.post('/users/', status_code=201, response_model=UserPublic)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
+    database.append(user_with_id)
+    return UserPublic(**user_with_id.model_dump())
--- tests/test_app.py
+++ tests/test_app.py
@@ -9,3 +9,20 @@ def test_root_deve_retornar_200_e_ola_mundo():
     response = client.get('/')
     assert response.status_code == 200
     assert response.json() == {'message': 'Olá Mundo!'}
+
+
+def test_create_user():
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'alice',
+            'email': 'alice@example.com',
+            'password': 'secret',
+        },
+    )
+    assert response.status_code == 201
+    assert response.json() == {
+        'username': 'alice',
+        'email': 'alice@example.com',
+        'id': 1,
+    }
